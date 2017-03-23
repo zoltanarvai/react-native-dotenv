b76c4977eb502f3b24bf569f9a8a5bd9d753aaf5
Zoltan Arvai <zoltan.arvai@live.com>
2017-03-23 18:35:21 +0700

    Use patch babel plugin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var config = require("react-native/packager/rn-cli.config");
 
 module.exports = {
   plugins: [
-    [require('babel-plugin-dotenv'), {
+    [require('./babel-plugin-dotenv'), {
       replacedModuleName: 'react-native-dotenv',
       configDir: config.getProjectRoots()[0]
     }],
